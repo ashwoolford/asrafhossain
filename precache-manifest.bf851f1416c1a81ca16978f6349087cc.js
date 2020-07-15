e6cbcd307349b9a38032fb34b5562f779f8ad53c
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2020-07-15 12:37:03 +0600

    Updates

---
+++ precache-manifest.bf851f1416c1a81ca16978f6349087cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e084cce2acf67adc06466a2289455f2",
+    "url": "/asrafhossain/index.html"
+  },
+  {
+    "revision": "fa18985cb65a0c7aba34",
+    "url": "/asrafhossain/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a9757ec6698dc185df6d",
+    "url": "/asrafhossain/static/css/main.682bfa76.chunk.css"
+  },
+  {
+    "revision": "fa18985cb65a0c7aba34",
+    "url": "/asrafhossain/static/js/2.4554851e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/asrafhossain/static/js/2.4554851e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9757ec6698dc185df6d",
+    "url": "/asrafhossain/static/js/main.84a06d29.chunk.js"
+  },
+  {
+    "revision": "ac8623cd7fb3432b71cd",
+    "url": "/asrafhossain/static/js/runtime-main.b1cdbff9.js"
+  }
+]);
